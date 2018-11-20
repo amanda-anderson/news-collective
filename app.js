39180991cf1d6dcce7a244cf324a06f6d9238ffd
Amanda <33987955+amanda-anderson@users.noreply.github.com>
2018-11-19 22:24:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-/* Wait for webpage to load before running js */
 $(document).ready(function() {
     $('#backgroundfade').fadeOut(1000);
 });
@@ -10,9 +9,12 @@ $(function() {
 });
 
 /* Load news articles dynamically */
+/* Initial */
+
+$('.wrapper').empty();
+
 $.ajax({
-  url:
-    "https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  url:"https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
   method: "GET",
   error: function() {
     console.log("broken");
@@ -45,9 +47,341 @@ function processData(data) {
         "</div ></a>"
     );
 
-    var $newDiv = $('<div id = "test" class=' + i + ' style=""></div>')
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
     $(".wrapper").append($newDiv);
     $("." + i).append($image, $author, $title, $description);
     console.log(artUrl);
   }
+}
+
+
+/* SVD */
+function fillSVD() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=svenska-dagbladet&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* Tech Crunch */
+function fillTechCrunch() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* WIRED */
+function fillWired() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=wired&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* ESPN */
+function fillESPN() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=espn&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* CNN */
+function fillCNN() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* CBC */
+function fillCBC() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
+}
+
+/* Al Jazeera */
+function fillAlJazeera() {
+
+$('.wrapper').empty();
+
+$.ajax({
+  url:"https://newsapi.org/v2/top-headlines?sources=al-jazeera-english&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
+  method: "GET",
+  error: function() {
+    console.log("broken");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+    var imgUrl = data.articles[i].urlToImage;
+    
+    if (description == null) continue;
+    
+    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
+    var $author = $('<div class="author">' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
+    $(".wrapper").append($newDiv);
+    $("." + i).append($image, $author, $title, $description);
+    console.log(artUrl);
+  }
+}
 }
