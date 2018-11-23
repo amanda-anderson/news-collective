7bd491ae35e20c1fd5c1f712ea8d3fc972873380
Amanda <anderson_amanda@mail.com>
2018-11-22 22:04:50 -0500

    Simplified fill function
    Removed redundant code

--- app.js
+++ app.js
@@ -9,56 +9,6 @@ $(function() {
 });
 
 /* Load news articles dynamically */
-/* Initial Articles CBC */
-$('.wrapper').empty();
-
-$.ajax({
-  url:"https://newsapi.org/v2/top-headlines?sources=" + 'cbc-news' + "&apiKey=3adf3d38e8da43b6bb01e6a321713c31",
-  method: "GET",
-  error: function() {
-    console.log("broken");
-  },
-  success: function(data) {
-    processData(data);
-  }
-});
-
-function processData(data) {
-  var articleItems = [];
-
-  for (var i = 0; i < data.articles.length; i++) {
-    var author = "CBC News";
-    var title = data.articles[i].title;
-    var description = data.articles[i].description;
-    var artUrl = data.articles[i].url;
-    var imgUrl = data.articles[i].urlToImage;
-    
-    if (imgUrl == null) {
-    	imgUrl = "newspaper.png";
-    }
-    
-    if (description == null) continue;
-    
-    var $image = $('<img class="image" src="' + imgUrl + '"' + "></img>");
-    var $author = $('<div class="author">' + author + "</div >");
-    var $title = $(
-      "<a href='" + artUrl + "'" + '><div class="title">' + title + "</div ></a>"
-    );
-    var $description = $(
-      "<a href=" +
-        artUrl +
-        '><div class="description">' +
-        description +
-        "</div ></a>"
-    );
-
-    var $newDiv = $('<div id = "dynamicDiv" class=' + i + ' style=""></div>')
-    $(".wrapper").append($newDiv);
-    $("." + i).append($image, $author, $title, $description);
-    console.log(artUrl);
-  }
-}
-
 /* Change Articles With User Selection */
 function fill(sourceSelection) {
 
